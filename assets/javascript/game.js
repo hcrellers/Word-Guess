e3617d1868274802652f134056333e448e8c4b76
hcrellers <halie.creollo@gmail.com>
2019-03-10 22:36:12 -0500

    maybe final draft

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,336 +1,160 @@
 var cartoons = [
-    "rugrats",
-    "heyarnold!",
-    "Doug",
-    "aaahhrealmonsters",
-    "rockosmodernlife",
-    "sailormoon",
-    "gargoyles",
-    "recess", 
-    "couragethecowardlydog",
-    "looneytunes",
-    "scoobydoo,"
-    
-   ]
-   
-   
-   var randomW = "";
-   var lettersOfWord = []
-   var blankSpace = 0;
-   var blankCorrect = [];
-   var wins = 0;
-   var lettersGuessed = [];
-   var guessesLeft = 13;
-     
+  "rugrats",
+  "heyarnold",
+  "Doug",
+  "aaahhrealmonsters",
+  "rockosmodernlife",
+  "sailormoon",
+  "gargoyles",
+  "recess",
+  "couragethecowardlydog",
+  "looneytunes",
+  "scoobydoo,"
 
-   
-   randomW = cartoons[Math.floor(Math.random() * cartoons.length)];
-   console.log(randomW)
-
-   lettersOfWord = randomW.split("");
-
-   blankSpace = lettersOfWord.length;
-   
-   for (var i = 0; i < blankSpace; i++) {
-   blankCorrect.push ("_");
-
-   }
-
-   document.onkeyup = function(event) {
-
-   
-   
-   document.getElementById("currentword").innerHTML = "  " + blankCorrect.join("  ");
-
-   console.log(randomW);
-   console.log(lettersOfWord);
-   console.log(blankSpace);
-   console.log(blankCorrect);
-
-   
-   
-   var r = document.getElementById("rugrats");
-   var heya = document.getElementById("heya");
-   var doug = document.getElementById("doug");
-   var aaahrm = document.getElementById("aaahrealmonsters");
-   var rockosml = document.getElementById("rockosmodernlife");
-   var sailorm = document.getElementById("sailormoon");
-   var gargoyles = document.getElementById("gargoyles");
-   var recess = document.getElementById("recess");
-   var couragetcd = document.getElementById("couragetcd");
-   var looneyt = document.getElementById("looneyt");
-   var scoobydoo = document.getElementById("scoobydoo");
-
-   
-
-   function aud() {
-    
-    if (randomWord === words[0]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.play();
-        document.getElementById("image").src = "./assets/images/rugrats.gif";
- 
-    }
-
-    else if (randomWord === words[1]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        r.pause();
-        heya.play();
-        document.getElementById("image").src = "./assets/images/heya.gif";
- 
-    }
-
-    else if (randomWord === words[2]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        heya.pause();
-        r.pause();
-        doug.play();
-        document.getElementById("image").src = "./assets/images/doug.gif";
- 
-    }
+]
 
 
-    else if (randomWord === words[3]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        aaahrm.play();
-        document.getElementById("image").src = "./assets/images/aaahhrm.gif";
- 
-    }
+var randomW = "";
+var lettersOfWord = []
+var blankSpace = 0;
+var blankCorrect = [];
+var wins = 0;
+var lettersGuessed = [];
+var guessesLeft = 13;
 
-    else if (randomWord === words[4]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        rockosml.play();
-        document.getElementById("image").src = "./assets/images/rocosml.gif";
- 
-    }
 
-    else if (randomWord === words[5]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        sailorm.play();
-        document.getElementById("image").src = "./assets/images/sailorm.gif";
- 
-    }
 
-    
-    else if (randomWord === words[6]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        recess.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        gargoyles.play();
-        document.getElementById("image").src = "./assets/images/gargoyles.gif";
- 
-    }
+randomW = cartoons[Math.floor(Math.random() * cartoons.length)];
+randomW = cartoons[0]; // TODO: static for testing
+console.log(randomW)
 
-    else if (randomWord === words[7]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        couragetcd.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        recess.play();
-        document.getElementById("image").src = "./assets/images/recess.gif";
- 
-    }
-    else if (randomWord === words[8]) {
-        scoobydoo.pause();
-        looneyt.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        couragetcd.play();
-        document.getElementById("image").src = "./assets/images/couragetcd.gif";
- 
-    }
+lettersOfWord = randomW.split("");
+console.log('letters-guessed', lettersOfWord);
 
-    else if (randomWord === words[9]) {
-        scoobydoo.pause();
-        couragetcd.pause();
-        recess.pause();
-        gargoyles.pause();
-        sailorm.pause();
-        rockosml.pause();
-        aaahrm.pause();
-        doug.pause();
-        heya.pause();
-        r.pause();
-        looneyt.play();
-        document.getElementById("image").src = "./assets/images/looneyt.gif";
- 
-    }
+blankSpace = lettersOfWord.length;
+console.log('blankSpace', blankSpace);
 
-    else if (randomWord === words[10]) {
-      looneyt.pause();
-      couragetcd.pause();
-      recess.pause();
-      gargoyles.pause();
-      sailorm.pause();
-      rockosml.pause();
-      aaahrm.pause();
-      doug.pause();
-      heya.pause();
-      r.pause();
-      scoobydoo.pause();
-      document.getElementById("image").src = "./assets/images/scoobyd.gif";
+for (var i = 0; i < blankSpace; i++) {
+  blankCorrect.push("_");
+}
+console.log('blankCorrect', blankCorrect);
 
-  }
+var r;
+var heya
+var doug;
+var aaahrm;
+var rockosml;
+var sailorm;
+var gargoyles;
+var recess;
+var couragetcd;
+var looneyt;
+var scoobydoo;
 
-  function reset() {
-    guessesLeft = 13;
-    lettersGuessed = [];
-    blankCorrect = [];
-    Game()
+
+r = document.getElementById("rugrats");
+heya = document.getElementById("heya");
+doug = document.getElementById("doug");
+aaahrm = document.getElementById("aaahrealmonsters");
+rockosml = document.getElementById("rockosmodernlife");
+sailorm = document.getElementById("sailormoon");
+gargoyles = document.getElementById("gargoyles");
+recess = document.getElementById("recess");
+couragetcd = document.getElementById("couragetcd");
+looneyt = document.getElementById("looneyt");
+scoobydoo = document.getElementById("scoobydoo");
+
+document.onkeyup = function (event) {
+
+  document.getElementById("currentword").innerHTML = "  " + blankCorrect.join("  ");
+
+  console.log(randomW);
+  console.log(lettersOfWord);
+  console.log(blankSpace);
+  console.log(blankCorrect);
+
+  var guessed = String.fromCharCode(event.keyCode).toLowerCase();
+  checkLetters(guessed);
+
+  complete();
+
+  console.log(guessed);
+
+  document.getElementById("letters-guessed").innerHTML = "  " + lettersGuessed.join(" ");
+
+}
+
+function reset() {
+  guessesLeft = 13;
+  lettersGuessed = [];
+  blankCorrect = [];
 }
 
 function checkLetters(letter) {
   var letterInWord = false;
   console.log('blankSpace', blankSpace);
   for (var i = 0; i < blankSpace; i++) {
-      if (randomW[i] == letter) {
-          letterInWord = true;
-      }
+    if (randomW[i] == letter) {
+      letterInWord = true;
+    }
   }
 
 
-if (letterInWord) {
-  
-  for (var i = 0; i < blankSpace; i++) {
-    console.log('inside letter check')
-    console.log(letter);
-    console.log(randomW[i]);
+  if (letterInWord) {
+
+    for (var i = 0; i < blankSpace; i++) {
+      console.log('letters-guessed')
+      console.log(letter);
+      console.log(randomW[i]);
       if (randomW[i] == letter) {
-          blankCorrect[i] = letter;
+        blankCorrect[i] = letter;
       }
+    }
   }
-}
 
-else {
-  lettersGuessed.push(letter);
-  guessesLeft--;
-}
+  else {
+    lettersGuessed.push(letter);
+    guessesLeft--;
+  }
 
-console.log(blankCorrect);
+  console.log(blankCorrect);
 
 }
 
 function complete() {
-  console.log("wins:" + wins + "| letters-guessed:" + lettersGuessed + "| guesses-left:" + guessesLeft)
+  console.log("wins:" + wins + "| letters-guessed:" + lettersGuessed + "| guesses-left:" + guessesLeft);
 
- 
+
   if (lettersOfWord.toString() == blankCorrect.toString()) {
-      wins++;
-      aud()
-      reset()
-    
-      document.getElementById("winstracker").innerHTML = " " + wins;
+    wins++;
+    aud();
+    reset();
 
-    } else if (guessesLeft === 0) {
-      lettersGuessed++;
-      reset()
-      document.getElementById("lettersguessed").innerHTML = " " + lettersGuessed;
+    document.getElementById("wins").innerHTML = " " + wins;
+
+  } else if (guessesLeft === 0) {
+    lettersGuessed++;
+    reset();
+    document.getElementById("letters-guessed").innerHTML = " " + lettersGuessed;
   }
   document.getElementById("currentword").innerHTML = "  " + blankCorrect.join(" ");
-  document.getElementById("guesses-left").innerHTML = " " + guessesLeft;
+   document.getElementById("guesses-left").innerHTML = " " + guessesLeft;
+    document.getElementById("wins").innerHTML = " " + wins;
 }
 
-Game()
 
 
-    var guessed = String.fromCharCode(event.keyCode).toLowerCase();
-    checkLetters(guessed);
+function aud() {
+
+    var ost = document.getElementById(randomW + "_ost")
+    ost.play();
+   // var titlePic = document.getElementById("titlePic")
+   // titlePic.src("./assets/images/" + randomW + ".jpg");
+}
  
-    complete();
-   
-    console.log(guessed);
 
-    document.getElementById("playerguesses").innerHTML = "  " + lettersGuessed.join(" ");
-
-
-   }
-
-  }
-
-
-
-
-
-
-
-   
-            
-        
-
-
-   
-  if ( navigator.platform.indexOf('Win') != -1 ) {
-    window.document.getElementById("wrapper").setAttribute("class", "windows");
-  } else if ( navigator.platform.indexOf('Mac') != -1 ) {
-    window.document.getElementById("wrapper").setAttribute("class", "mac");
-  }
+if (navigator.platform.indexOf('Win') != -1) {
+  window.document.getElementById("wrapper").setAttribute("class", "windows");
+} else if (navigator.platform.indexOf('Mac') != -1) {
+  window.document.getElementById("wrapper").setAttribute("class", "mac");
+}
